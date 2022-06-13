b4592734b68f704323ab442a61d2c87a5d307bdf
Gabriel Trucollo <gabrieltrucollo@gmail.com>
2022-06-13 19:47:07 -0300

    tests: new cases in tests

--- BrazilianDocuments.Tests/Documents/CpfTest.cs
+++ BrazilianDocuments.Tests/Documents/CpfTest.cs
@@ -2,20 +2,32 @@ namespace Tests.Documents;
 
 public class CpfTest
 {
-    [Fact(DisplayName = "Shoud all caracteres are equal is valid")]
-    public void TestShouldAllCharactersAreEqualIsValid()
+    [Theory(DisplayName = "Shoud all caracteres are equal is valid")]
+    [InlineData("11111111111")]
+    [InlineData("22222222222")]
+    [InlineData("33333333333")]
+    [InlineData("44444444444")]
+    [InlineData("55555555555")]
+    [InlineData("66666666666")]
+    [InlineData("77777777777")]
+    [InlineData("88888888888")]
+    [InlineData("99999999999")]
+    public void TestShouldAllCharactersAreEqualIsValid(string value)
     {
-        Cpf document = new Cpf("11111111111");
+        Cpf document = new Cpf(value);
 
         Assert.True(document.IsValid());
     }
 
-    [Fact(DisplayName = "Shoud be return correctly masked value")]
-    public void TestSholdBeReturnCorrectlyMaskedValue()
-    {
-        Cpf document = new Cpf("11111111111");
+    [Theory(DisplayName = "Shoud be return correctly masked value")]
+    [InlineData("11111111111", "111.111.111-11")]
+    [InlineData("37544195015", "375.441.950-15")]
 
-        Assert.Equal("111.111.111-11", document.Mask());
+    public void TestSholdBeReturnCorrectlyMaskedValue(string unmask, string expected)
+    {
+        Cpf document = new Cpf(unmask);
+
+        Assert.Equal(expected, document.Mask());
     }
 
     [Fact(DisplayName = "Shoud be return invalid to a incorrect document")]
